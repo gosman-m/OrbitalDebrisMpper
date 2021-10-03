a73261b24a4e0c90bc7e92b0223a7e82f84a21e8
gosman-m <gosman.mihail@gmail.com>
2021-10-03 20:14:39 +0300

    update

--- odm.py
+++ odm.py
@@ -1,6 +1,20 @@
+from pygame.version import PygameVersion
 from modules import datahandler
 import pygame
 import ctypes
+
+
+class Button:
+    def __init__(self, path):
+        self.surface = pygame.image.load(path)
+        self.hover = False
+    
+    def update(self):
+        if self.hover == True:
+            self.surface.set_alpha(255)
+        else:
+            self.surface.set_alpha(130)
+
 
 def main():
     
@@ -21,10 +35,12 @@ def main():
     map_on = True
     count = 0
 
-    world_map = pygame.image.load('assets/world map blur.jpg')
+    mousex, mousey = 0, 0
+
+    world_map = pygame.image.load('assets/world map.jpg')
     world_map = pygame.transform.scale(world_map, [1280, 720])
-    focus = pygame.image.load('assets/focus.png')
-    grid = pygame.image.load('assets/grid.png')
+
+  
 
     font = pygame.font.SysFont('arial', 14)
 
@@ -49,7 +65,7 @@ def main():
 
         # Draw debris
         for object in data.debris_list:
-            pygame.draw.circle(surface, [255, 0, 0], [object.x, object.y], 2)
+            pygame.draw.circle(surface, [255, 0, 0], [object.x, object.y], 1)
             
         '''for x in range(0, 1280, int(1280/18)):
             pygame.draw.line(surface, [0, 0, 0, 255], [x, 0], [x, 720], 1)
@@ -57,13 +73,13 @@ def main():
             pygame.draw.line(surface, [0, 0, 0], [0, x], [1280, x], 1)
         '''
 
-        # Draw buttons
-        # surface.blit(focus, [500, 660])
-        # surface.blit(grid, [570, 660])
+        # Draw abtion bar
+
 
         pygame.display.update()
 
         # UPDATE ~~~~~~~~~~~~
+        
         if count == 0:
             for i in range(0, int(len(data.debris_list) / 3)):
                 data.update_location(data.debris_list[i])
@@ -79,12 +95,24 @@ def main():
                 data.update_location(data.debris_list[i])
             count = 0
 
+        # Obtion bar update
+        
 
         # INPUT ~~~~~~~~~~~~~~~~
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
     
+        '''mousex, mousey = pygame.mouse.get_pos()
+        if mousex >= 500 and mousex <= 700:
+            if mousey >= 660 and mousey <= 700:
+                togle_map.hover = True
+            else:
+                togle_map.hover = False
+        else:
+            togle_map.hover = False
+        '''
+
         print(clock.tick(30))
     exit()
     # END
